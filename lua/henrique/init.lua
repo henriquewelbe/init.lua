c9160ce520b88164f271fa1831f15e0b92581d85
Henrique Welbe <henriquewelbe@gmail.com>
2026-04-05 14:30:47 -0300

    fix init file

--- lua/henrique/init.lua
+++ lua/henrique/init.lua
@@ -1,3 +1,45 @@
 require("henrique.set")
 require("henrique.lazy")
 require("henrique.remap")
+
+local augroup = vim.api.nvim_create_augroup
+
+local HenriqueGroup = augroup('Henrique', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})
+
+
+autocmd({"BufWritePre"}, {
+    group = HenriqueGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
+
+
+autocmd('LspAttach', {
+    group = HenriqueGroup,
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+    end
+})
+
+
+
